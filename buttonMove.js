c488d7ce08ca2cc8736197b9f9424d043ec81fbc
Binghagoon <foat3376@korea.ac.kr>
2021-07-08 16:05:27 +0900

    modifying html and seperate css file

---
+++ buttonMove.js
@@ -0,0 +1,13 @@
+function GotoReservation(){
+    window.location = "Reservation.html";
+}
+function GotoMap(){
+    window.location = "Map.html";
+}
+function GotoMyPage(){
+    window.location = "My Page.html";
+}
+function GotoAlarm(){
+    window.location = "Alarm.html";
+}
+/* Map Alarm Login Page My Page의 동적움직임을 정의 */
