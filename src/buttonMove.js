e73b6cabbaf40b27a01ae367c215d31ba862bbef
Bingha <foat3376@korea.ac.kr>
2021-08-20 15:55:48 +0900

    reprogramming button move via array

--- src/buttonMove.js
+++ src/buttonMove.js
@@ -1,39 +1,28 @@
 var ifr;
 var buttonParent;
 var now = 2;
+var buttons = [];
+var pages = [];
 
 $(function(){
     ifr =  $("#Iframe");
     buttonParent = $("FrameSuffix");
-    $("#ReservationHome").on("click",GotoReservation);
-    $("#MapHome").on("click",GotoMap);
-    $("#NoticeHome").on("click",GotoAlarm);
-    $("#MyInfoHome").on("click",GotoMyPage);
+    buttons = [ undefined , $("#ReservationHome"), $("#MapHome"), $("#NoticeHome"), $("#MyInfoHome") ]
+    pages = [ undefined, "Reservation-Driver", "Map", "Alarm", "MyPage-Driver" ]
+    buttons[1].on("click",() => IframeChange(1));
+    buttons[2].on("click",() => IframeChange(2));
+    buttons[3].on("click",() => IframeChange(3));
+    buttons[4].on("click",() => IframeChange(4));
 });
 
 function ChangeSelectClass(to){
-    $("div#FrameSuffix Button:nth-child(" + to +")").addClass("select");
-    $("div#FrameSuffix Button:nth-child(" + now +")").removeClass("select");
+    buttons[to].addClass("select");
+    buttons[now].removeClass("select");
     now = to;
-    debugger;
+}
+function IframeChange(to){
+    ifr.attr("src", pages[to] + ".html");
+    ChangeSelectClass(to);
 }
 
-function GotoReservation(){
-    ifr.attr("src", "Reservation-Driver.html");
-    ChangeSelectClass(1);
-}
-function GotoMap(){
-    ifr.attr("src", "Map.html");
-    ChangeSelectClass(2);
-
-}
-function GotoMyPage(){
-    ifr.attr("src", "MyPage-Driver.html");
-    ChangeSelectClass(4);
-
-}
-function GotoAlarm(){
-    ifr.attr("src", "Alarm.html");
-    ChangeSelectClass(3);
-}
 /* Frame의 동적움직임을 정의 */
