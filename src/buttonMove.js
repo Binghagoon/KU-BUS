3765168161e45fafdb02da0877ae67db51c193c9
Bingha <foat3376@korea.ac.kr>
2021-08-22 21:33:45 +0900

    Update Frame's Action

--- src/buttonMove.js
+++ src/buttonMove.js
@@ -3,26 +3,29 @@ var buttonParent;
 var now = 2;
 var buttons = [];
 var pages = [];
+var pre =
 
 $(function(){
     ifr =  $("#Iframe");
     buttonParent = $("FrameSuffix");
     buttons = [ undefined , $("#ReservationHome"), $("#MapHome"), $("#NoticeHome"), $("#MyInfoHome") ]
-    pages = [ undefined, "Reservation-Driver", "Map", "Alarm", "MyPage-Driver" ]
-    buttons[1].on("click",() => IframeChange(1));
-    buttons[2].on("click",() => IframeChange(2));
-    buttons[3].on("click",() => IframeChange(3));
-    buttons[4].on("click",() => IframeChange(4));
+    pages = [ undefined, "Reservation", "Map", "Alarm", "MyPage" ]
+    pre = { "STUDENT" : "Student", "DRIVER" : "Driver"};
+    buttons[1].on("click",() => Click(1));
+    buttons[2].on("click",() => Click(2));
+    buttons[3].on("click",() => Click(3));
+    buttons[4].on("click",() => Click(4));
 });
 
-function ChangeSelectClass(to){
+function Click(to){
+    var src = pages[to] + ".html";
+    if(to == 1 || to == 2){
+        src = pre[arg["role"]] + "/" + src;
+    }
+    ifr.attr("src", src);
     buttons[to].addClass("select");
     buttons[now].removeClass("select");
     now = to;
-}
-function IframeChange(to){
-    ifr.attr("src", pages[to] + ".html");
-    ChangeSelectClass(to);
 }
 
 /* Frame의 동적움직임을 정의 */
