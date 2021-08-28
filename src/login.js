c266793298123336cd9bd6c62e875857f8f8436e
Bingha <foat3376@korea.ac.kr>
2021-08-28 22:47:18 +0900

    seperate url parameter function

--- src/login.js
+++ src/login.js
@@ -5,28 +5,6 @@ var Position  = {  //사용자 타입의 ENUM
     ADMIN: 3,
     ETC: 4
 }
-/**
- * http://stackoverflow.com/a/10997390/11236
- */
-function updateURLParameter(url, param, paramVal){
-    var newAdditionalURL = "";
-    var tempArray = url.split("?");
-    var baseURL = tempArray[0];
-    var additionalURL = tempArray[1];
-    var temp = "";
-    if (additionalURL) {
-        tempArray = additionalURL.split("&");
-        for (var i=0; i<tempArray.length; i++){
-            if(tempArray[i].split('=')[0] != param){
-                newAdditionalURL += temp + tempArray[i];
-                temp = "&";
-            }
-        }
-    }
-
-    var rows_txt = temp + "" + param + "=" + paramVal;
-    return baseURL + "?" + newAdditionalURL + rows_txt;
-}
 
 function PageChange(position){  //position = {id: String, name: String, no: Int}
     if(position["name"] == "DRIVER"){
