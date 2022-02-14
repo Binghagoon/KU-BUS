4464ce6e79b014880f28ce8b49bbcc74b11bd203
binghagoon <foat3376@korea.ac.kr>
2022-02-14 21:02:10 +0900

    garbage collect and seperate marker work

--- src/Map.js
+++ src/Map.js
@@ -1,8 +1,6 @@
 
 var recordList;
 var map
-var markers = [];
-
 
 var mylat, mylng;
 var from={
@@ -15,11 +13,10 @@ var to={
     "lng": Intl,
     "name": String
 };
+
 var fromclicked = false,
     toclicked = false;
 var clickblocked = false;
-var StarMarkerSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png"; 
-var imageSize = new kakao.maps.Size(24, 35);
 
 function StartMap(callback) {
     var getpos = new Promise(function(resolve, reject){
@@ -39,20 +36,14 @@ function StartMap(callback) {
 }
 
 function successCallback(position) {
-//    var args = top.args;
     var coord = position["coords"];
     var lat = coord["latitude"];
     var lng = coord["longitude"];
-//    mylat = lat;
-//    mylng = lng;
-//    from["lat"] = lat;
-//    from["lng"] = lng;
-    //To Be cleared 09.14 TBDJS
     //Anam station
         lat = 37.586232954034564;
         lng = 127.02928291766814;
     //end
-    $("#set_location").html('latitude : ' + lat + ', longitude : ' + lng);
+
     var container = document.getElementById('Map');
     var options = {
         center: new kakao.maps.LatLng(lat, lng),
@@ -67,7 +58,6 @@ function RecordPositionGet(){
             url:  window.location.origin + "/node/record-position",
             type: "GET",
             error:function(request,status,error){
-                //alert("code:"+request.status+"\n"+"message:"+request.responseText+"\n"+"error:"+error);
                 alert("record position get error");
             },
             success: function(data, status, xhr){
@@ -87,32 +77,7 @@ function RecordPositionGet(){
         });
     }
 }
-var UserMarker = {
-    'DRIVER': null,
-    'STUDENT': null,
-};
 
-function MarkerCreate(position, who){
-    const imgconvert={
-        'DRIVER': window.location.origin + '/src/img/car-pin.png',
-        'STUDENT': window.location.origin + '/src/img/human-pin.png',
-        null : null,
-        undefined : null,
-    }
-    var marker;
-    var v = {};
-    v['lat']= position['latitude'];
-    v['lng']= position['longitude'];
-
-    marker = CreateMarker(imgconvert[who], v, null);
-    return marker;
-
-}
-
-function MarkerLocationChange(marker, position){
-    marker.setPosition(new kakao.maps.LatLng(position['latitude'], position['longitude']))
-
-}
 function Pinupdate(position, who){
     if(UserMarker[who] == null){
         UserMarker[who] = MarkerCreate(position, who);
@@ -128,75 +93,6 @@ async function TraceAnother(id, who){
 async function UpdateAnother(id, who){
     var result = await top.GetPositionById(id);
     Pinupdate(result,who);
-}
-
-function CreateMarker(imgSrc, value, event){
-    // img : String value : Object
-    var imageSize = new kakao.maps.Size(24,35);
-    var markerImage = new kakao.maps.MarkerImage(imgSrc, imageSize);
-    return new kakao.maps.Marker({
-        map: map, // 마커를 표시할 지도
-        position: new kakao.maps.LatLng(value.lat,value.lng), // 마커를 표시할 위치
-        title : value.name, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-        image : markerImage, // 마커 이미지 
-    });
-}
-
-var orderStat=['from','to','reserve'];
-var statnum =0;
-var imgChangedMarker = null;
-var deletedMarker = [];
-
-function MarkerImageChange(what ,marker){
-    if(!marker) return;
-    var imgsrc = what=='star'? StarMarkerSrc : 'https://t1.daumcdn.net/mapjsapi/images/marker.png'
-    marker.setImage(new kakao.maps.MarkerImage(imgsrc, imageSize));
-    imgChangedMarker = marker;
-}
-function ClickedMarkerDelete(){
-    imgChangedMarker.setMap(null);
-    deletedMarker.push(imgChangedMarker);
-}
-
-function DeletedMarkerRevive(){
-    deletedMarker.forEach(function(value, index){
-        value.setMap(map);
-    });
-}
-
-var openedIwcontent = null;
-function MarkerClickEvent(value, marker, imageSize){
-    function InfowindowOpen(status){
-        var iwContent = "", // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
-            iwRemoveable = true; // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
-        $("#PositionName").html(value.name)
-
-        if(openedIwcontent){
-            openedIwcontent.close();
-        }
-        iwContent = $("#InfowindowTemplete").html();
-        var infowindow = new kakao.maps.InfoWindow({
-            content : iwContent,
-            removable : iwRemoveable
-        });   
-        infowindow.open(map, marker);
-        return infowindow;
-    }
-    function OrderInput(status){
-        if(status == orderStat[0]){      //from
-            from = value;
-
-        } else if(status == orderStat[1]){      //to
-            to = value;
-        } else{
-            console.log("Too many click");            
-        }
-    }
-    MarkerImageChange('star', imgChangedMarker);
-    MarkerImageChange(null, marker);
-    NextButtonSwitch(true);
-    OrderInput(orderStat[statnum]);
-    openedIwcontent = InfowindowOpen(orderStat[statnum]);
 }
 
 function NextStat(){
@@ -215,24 +111,11 @@ function MapPinWithRecord(data){
     } else{
         recordList = data;
     }
-    //var bluepin = window.location.origin + "/src/img/blue-map-pin.png";
-    //var redpin =  window.location.origin + "/src/img/red-map-pin.png";
-
-    
-
 
     recordList.forEach(function(value, index){
-        //refernce with https://apis.map.kakao.com/web/sample/multipleMarkerImage/
-
-        // 마커 이미지의 이미지 크기 입니다
-        //var imageSize = new kakao.maps.Size(24, 35);
-        
-
-        // 마커를 생성합니다
         var marker = CreateMarker(StarMarkerSrc, value, null);
         markers.push(marker);
         marker.setMap(map);
         kakao.maps.event.addListener(marker, 'click', ()=>MarkerClickEvent(value, marker, imageSize));
     });
 }
-
---
+++ src/Marker.js
@@ -0,0 +1,101 @@
+
+
+var markers = [];
+
+var UserMarker = {
+    'DRIVER': null,
+    'STUDENT': null,
+};
+var StarMarkerSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png"; 
+var imageSize = new kakao.maps.Size(24, 35);
+var orderStat=['from','to','reserve'];
+var statnum =0;
+var imgChangedMarker = null;
+var deletedMarker = [];
+var openedIwcontent = null;
+
+function MarkerCreate(position, who){
+    const imgconvert={
+        'DRIVER': window.location.origin + '/src/img/car-pin.png',
+        'STUDENT': window.location.origin + '/src/img/human-pin.png',
+        null : null,
+        undefined : null,
+    }
+    var marker;
+    var v = {};
+    v['lat']= position['latitude'];
+    v['lng']= position['longitude'];
+
+    marker = CreateMarker(imgconvert[who], v, null);
+    return marker;
+
+}
+
+function MarkerLocationChange(marker, position){
+    marker.setPosition(new kakao.maps.LatLng(position['latitude'], position['longitude']))
+}
+
+
+function CreateMarker(imgSrc, value, event){
+    // img : String value : Object
+    var imageSize = new kakao.maps.Size(24,35);
+    var markerImage = new kakao.maps.MarkerImage(imgSrc, imageSize);
+    return new kakao.maps.Marker({
+        map: map, // 마커를 표시할 지도
+        position: new kakao.maps.LatLng(value.lat,value.lng), // 마커를 표시할 위치
+        title : value.name, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+        image : markerImage, // 마커 이미지 
+    });
+}
+
+function ClickedMarkerDelete(){
+    imgChangedMarker.setMap(null);
+    deletedMarker.push(imgChangedMarker);
+}
+
+function DeletedMarkerRevive(){
+    deletedMarker.forEach(function(value, index){
+        value.setMap(map);
+    });
+}
+
+function MarkerImageChange(what ,marker){
+    if(!marker) return;
+    var imgsrc = what=='star'? StarMarkerSrc : 'https://t1.daumcdn.net/mapjsapi/images/marker.png'
+    marker.setImage(new kakao.maps.MarkerImage(imgsrc, imageSize));
+    imgChangedMarker = marker;
+}
+
+function MarkerClickEvent(value, marker, imageSize){
+    function InfowindowOpen(status){
+        var iwContent = "", // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+            iwRemoveable = true; // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
+        $("#PositionName").html(value.name)
+
+        if(openedIwcontent){
+            openedIwcontent.close();
+        }
+        iwContent = $("#InfowindowTemplete").html();
+        var infowindow = new kakao.maps.InfoWindow({
+            content : iwContent,
+            removable : iwRemoveable
+        });   
+        infowindow.open(map, marker);
+        return infowindow;
+    }
+    function OrderInput(status){
+        if(status == orderStat[0]){      //from
+            from = value;
+
+        } else if(status == orderStat[1]){      //to
+            to = value;
+        } else{
+            console.log("Too many click");            
+        }
+    }
+    MarkerImageChange('star', imgChangedMarker);
+    MarkerImageChange(null, marker);
+    NextButtonSwitch(true);
+    OrderInput(orderStat[statnum]);
+    openedIwcontent = InfowindowOpen(orderStat[statnum]);
+}
