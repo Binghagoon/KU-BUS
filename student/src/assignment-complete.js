d3c2a0413716af9e6bdd56a220a1b21bd23e5d7a
Daeseong Shin <esc990720@korea.ac.kr>
2022-02-28 16:23:00 +0900

    Fix promise problem

--- student/src/assignment-complete.js
+++ student/src/assignment-complete.js
@@ -4,15 +4,16 @@ const query = queryToObject();
 const studentLocation = new UserLocation(sessionStorage.getItem("kubus_member_id"));
 let driverID = query["driverId"];
 let studentID = sessionStorage.getItem("kubus_member_id");
+
 let clicked = 0;
 
 $(function () {
   window.onbeforeunload = cancelCall;
   startMap(function () {
-    traceAnother(driverID, "DRIVER");
+    studentLocation.awakeInterval(1000, pos => pinUpdate(pos, "STUDENT"));
   });
 
-  studentLocation.awakeInterval(1000, pos => pinUpdate(pos, "STUDENT"));
+  traceAnother(driverID, "DRIVER");
 
   $("#cancel-reservation").on("click", cancelCall);
   $("#send-message").on("click", function (e) {
