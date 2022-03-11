64a5175c64f687ecec08a07a482b06a248a27a47
Merseong <esc990720@korea.ac.kr>
2022-03-11 15:40:47 +0900

    Add checking call status in student

--- student/src/assignment-complete.js
+++ student/src/assignment-complete.js
@@ -8,6 +8,7 @@ let driverID = query["driverid"];
 let studentID = sessionStorage.getItem("kubus_member_id");
 
 let clicked = 0;
+let checkCallStatusIntervalId = -1;
 
 $(document).ready(function () {
   window.onbeforeunload = cancelCall;
@@ -17,6 +18,8 @@ $(document).ready(function () {
   });
 
   traceAnother(driverID, "DRIVER");
+
+  checkCallStatusIntervalId = setInterval(checkCallStatus, 3000);
 
   $("#cancel-call").on("click", cancelCall);
   $("#send-message").on("click", function (e) {
@@ -28,17 +31,66 @@ $(document).ready(function () {
 
 function cancelCall() {
   $.ajax({
-    url: "../node/call-cancel",
+    url: "/node/call-cancel",
+    type: "POST",
     data: {
-      id: query["callNo"],
+      callNo: query["callNo"],
     },
     error: function (jqXHR, textStatus, errorThrown) {
-      console.log("Error");
+      console.error("Error in cancel call");
     },
     success: function (data, textStatus, jqXHR) {
-      alert("취소가 완료되었습니다.");
-      console.log("Move to first-page");
+      if (data.status != "success") {
+        console.error(data.errorMessage);
+      } else {
+        alert("취소가 완료되었습니다.");
+        clearInterval(checkCallStatusIntervalId);
+        deleteLocation();
+      }
       window.location.href = "first-page.html";
     },
+  });
+}
+
+function deleteLocation() {
+  $.ajax({
+    url: "/node/location-delete",
+    type: "POST",
+    async: false,
+    data: {
+      id: sessionStorage.getItem("kubus_member_id")
+    },
+    error: function (jqXHR, textStatus, errorThrown) {
+      console.error("Error in delete Location");
+    },
+    success: function (data, textStatus, jqXHR) {
+      if (data.status != "success") {
+        console.error(data.errorMessage);
+      }
+    },
+  })
+}
+
+function checkCallStatus() {
+  $.ajax({
+    url: "/node/call-status",
+    type: "GET",
+    data: {
+      callNo: query["callNo"]
+    },
+    error: function (jqXHR, textStatus, errorThrown) {
+      console.error("Error in get call status");
+      clearInterval(checkCallStatusIntervalId);
+    },
+    success: function (data, textStatus, jqXHR) {
+      if (data.callStatus == 3) { // 운행중
+        $("#minute-left").html("<h4>운행중입니다.</h4>");
+      } else if (data.callStatus == 4) { // 운행완료
+        $("#minute-left").html("<h4>운행이 완료되었습니다.</h4>");
+        setTimeout(() => {
+          window.location.href = "first-page.html";
+        }, 5000);
+      }
+    }
   });
 }
