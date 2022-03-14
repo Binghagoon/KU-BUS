8abc22eadaf60c567df12097adafda72cc117814
Merseong <esc990720@korea.ac.kr>
2022-03-14 18:28:36 +0900

    Fix checkCall action in student

--- student/src/doing-call.js
+++ student/src/doing-call.js
@@ -34,15 +34,19 @@ function checkCall() {
         alert("failed on check reservation");
       },
       success: function (data, status, xhr) {
-        if (data == undefined) return;
-        if (data.callSuccess != false) {
-          if (data.callSuccess) {
-            query["driverid"] = data.driverID;
+        if (data.status && data.status == "error") {
+          alert("배차에 실패했습니다. 잠시 후 다시 시도해주세요.");
+          console.error(data.message);
+          cancelCall();
+          return;
+        }
+        if (data.callStatus) {
+          if (data.callSuccess && data.driverid) {
+            query["driverid"] = data.driverid;
             clearInterval(checkIntervalId);
             successCall();
           } else {
-            alert("배차에 실패했습니다. 잠시 후 다시 시도해주세요.");
-            cancelCall();
+            return;
           }
         }
       },
